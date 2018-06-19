518ee58dba73d59c7fd7d59ca7bb6918f4991d85
RafaelShouman <rafael@shouman.com.br>
2018-06-19 11:35:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
+	///Impressão do JSON no HTML
 	$.getJSON('produtos.json', function(data){
-			
+
 		this.qtd = data.produtos.length;
 		this.retorno = '';
 
 		for(i in data.produtos){
-	    	this.retorno += "<tr class='clickable el-alvo'><td>Nome: " + data.produtos[i].nome + '</td>';
-	    	this.retorno += '<td>ID: ' + data.produtos[i].id + '</td></tr>';
-	    }						
+				this.retorno += "<tr class='clickable el-alvo'>";
+	    	this.retorno += "<td>" + data.produtos[i].nome + "</td>";
+	    	this.retorno += "<td>" + data.produtos[i].id + "</td>";
+	    	this.retorno += "</tr>";
+	    }
 
 		$('#resultado').html(this.retorno);
 		});
@@ -17,3 +20,16 @@ $(document).ready(function() {
 		});
 
 });
+///Validação dos campos núméricos
+function validaCamposSomenteNumero() {
+  var inputs = document.getElementsByClassName("somente-numero");
+  for (var i = 0; i < inputs.length; ++i) {
+      var input = inputs[i];
+      if (input.value !== "") {
+          if (isNaN(input.value)) {
+              alert("Preencha apenas com números!");
+              return;
+          }
+      }
+  }
+}
